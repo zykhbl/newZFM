1f926969187bfe27945926a583fb91568ce62f1f
zykhbl@126.com <2002125152zyk>
2016-03-11 18:14:45 +0800

    提交代码

--- zFM/audio/decode/mp3/side_info.h
+++ zFM/audio/decode/mp3/side_info.h
@@ -11,29 +11,33 @@
 
 #include "frame.h"
 
+struct gr_info_s {
+    unsigned part2_3_length;
+    unsigned big_values;
+    unsigned global_gain;
+    unsigned scalefac_compress;
+    unsigned window_switching_flag;
+    unsigned block_type;
+    unsigned mixed_block_flag;
+    unsigned table_select[3];
+    unsigned subblock_gain[3];
+    unsigned region0_count;
+    unsigned region1_count;
+    unsigned preflag;
+    unsigned scalefac_scale;
+    unsigned count1table_select;
+};
+
+struct ch_info_s {
+    unsigned scfsi[4];
+    struct gr_info_s gr[2];
+};
+
 //Layer III side information
 struct III_side_info {
     unsigned main_data_begin;
     unsigned private_bits;
-    struct {
-        unsigned scfsi[4];
-        struct gr_info_s {
-            unsigned part2_3_length;
-            unsigned big_values;
-            unsigned global_gain;
-            unsigned scalefac_compress;
-            unsigned window_switching_flag;
-            unsigned block_type;
-            unsigned mixed_block_flag;
-            unsigned table_select[3];
-            unsigned subblock_gain[3];
-            unsigned region0_count;
-            unsigned region1_count;
-            unsigned preflag;
-            unsigned scalefac_scale;
-            unsigned count1table_select;
-        } gr[2];
-    } ch[2];
+    struct ch_info_s ch[2];
 };
 
 extern struct III_side_info*create_III_side_info();
